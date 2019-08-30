35a6f349e89db08bbc75b24d6992a7341b86235c
noellabo <noel.yoshiba@gmail.com>
2019-08-30 17:02:41 +0900

    Add subscribe features

--- app/models/concerns/account_associations.rb
+++ app/models/concerns/account_associations.rb
@@ -60,8 +60,15 @@ module AccountAssociations
     # Hashtags
     has_and_belongs_to_many :tags
     has_many :featured_tags, -> { includes(:tag) }, dependent: :destroy, inverse_of: :account
+    has_many :follow_tags, -> { includes(:tag) }, dependent: :destroy, inverse_of: :account
 
     # Account deletion requests
     has_one :deletion_request, class_name: 'AccountDeletionRequest', inverse_of: :account, dependent: :destroy
+
+    # KeywordSubscribes
+    has_many :keyword_subscribes, inverse_of: :account, dependent: :destroy
+
+    # DomainSubscribes
+    has_many :domain_subscribes, inverse_of: :account, dependent: :destroy
   end
 end
