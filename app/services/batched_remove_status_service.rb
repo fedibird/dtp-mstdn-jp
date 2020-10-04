mirielnet/rdon

Add a domain timeline

app/services/batched_remove_status_service.rb
@@ -16,6 +16,7 @@ class BatchedRemoveStatusService < BaseService
 
     @mentions = statuses.each_with_object({}) { |s, h| h[s.id] = s.active_mentions.includes(:account).to_a }
     @tags     = statuses.each_with_object({}) { |s, h| h[s.id] = s.tags.pluck(:name) }
+    @domains  = statuses.each_with_object({}) { |s, h| h[s.id] = s.account.domain unless s.local? }
 
     @json_payloads = statuses.each_with_object({}) { |s, h| h[s.id] = Oj.dump(event: :delete, payload: s.id.to_s) }
 
@@ -73,10 +74,16 @@ class BatchedRemoveStatusService < BaseService
     redis.pipelined do
       redis.publish('timeline:public', payload)
       redis.publish('timeline:public:local', payload) if status.local?
+      @domains[status.id].each do |domain|
+        redis.publish("timeline:public:domain:#{domain.mb_chars.downcase}", payload)
+      end
 
       if status.media_attachments.any?
         redis.publish('timeline:public:media', payload)
         redis.publish('timeline:public:local:media', payload) if status.local?
+        @domains[status.id].each do |domain|
+          redis.publish("timeline:public:domain:media:#{domain.mb_chars.downcase}", payload)
+        end
       end
 
       @tags[status.id].each do |hashtag|

app/services/fan_out_on_write_service.rb
@@ -162,14 +162,22 @@ class FanOutOnWriteService < BaseService
     Rails.logger.debug "Delivering status #{status.id} to public timeline"
 
     Redis.current.publish('timeline:public', @payload)
-    Redis.current.publish('timeline:public:local', @payload) if status.local?
+    if status.local?
+      Redis.current.publish('timeline:public:local', @payload)
+    else
+      Redis.current.publish("timeline:public:domain:#{status.account.domain.mb_chars.downcase}", @payload)
+    end
   end
 
   def deliver_to_media(status)
     Rails.logger.debug "Delivering status #{status.id} to media timeline"
 
     Redis.current.publish('timeline:public:media', @payload)
-    Redis.current.publish('timeline:public:local:media', @payload) if status.local?
+    if status.local?
+      Redis.current.publish('timeline:public:local:media', @payload)
+    else
+      Redis.current.publish("timeline:public:domain:media:#{status.account.domain.mb_chars.downcase}", @payload)
+    end
   end
 
   def deliver_to_own_conversation(status)

app/services/remove_status_service.rb
@@ -140,14 +140,22 @@ class RemoveStatusService < BaseService
     return unless @status.public_visibility?
 
     redis.publish('timeline:public', @payload)
-    redis.publish('timeline:public:local', @payload) if @status.local?
+    if @status.local?
+      redis.publish('timeline:public:local', @payload)
+    else
+      redis.publish("timeline:public:domain:#{@account.domain.mb_chars.downcase}", @payload)
+    end
   end
 
   def remove_from_media
     return unless @status.public_visibility?
 
     redis.publish('timeline:public:media', @payload)
-    redis.publish('timeline:public:local:media', @payload) if @status.local?
+    if @status.local?
+      redis.publish('timeline:public:local:media', @payload)
+    else
+      redis.publish("timeline:public:domain:media:#{@account.domain.mb_chars.downcase}", @payload)
+    end
   end
 
   def remove_media

fedibird/dtp-mstdn-jp

app/services/batched_remove_status_service.rb
@@ -16,6 +16,7 @@ class BatchedRemoveStatusService < BaseService
 
     @mentions = statuses.each_with_object({}) { |s, h| h[s.id] = s.active_mentions.includes(:account).to_a }
     @tags     = statuses.each_with_object({}) { |s, h| h[s.id] = s.tags.pluck(:name) }
+    @domains  = statuses.each_with_object({}) { |s, h| h[s.id] = s.account.domain unless s.local? }
 
     @json_payloads = statuses.each_with_object({}) { |s, h| h[s.id] = Oj.dump(event: :delete, payload: s.id.to_s) }
 
@@ -76,6 +77,7 @@ class BatchedRemoveStatusService < BaseService
         redis.publish('timeline:public:local', payload)
       else
         redis.publish('timeline:public:remote', payload)
+        redis.publish("timeline:public:remote:#{domain.mb_chars.downcase}", payload)
       end
       if status.media_attachments.any?
         redis.publish('timeline:public:media', payload)
@@ -83,6 +85,7 @@ class BatchedRemoveStatusService < BaseService
           redis.publish('timeline:public:local:media', payload)
         else
           redis.publish('timeline:public:remote:media', payload)
+          redis.publish("timeline:public:remote:media:#{domain.mb_chars.downcase}", payload)
         end
       end
 

app/services/fan_out_on_write_service.rb
@@ -185,6 +185,7 @@ class FanOutOnWriteService < BaseService
       Redis.current.publish('timeline:public:local', @payload)
     else
       Redis.current.publish('timeline:public:remote', @payload)
+      Redis.current.publish("timeline:public:remote:#{status.account.domain.mb_chars.downcase}", @payload)
     end
   end
 
@@ -196,6 +197,7 @@ class FanOutOnWriteService < BaseService
       Redis.current.publish('timeline:public:local:media', @payload)
     else
       Redis.current.publish('timeline:public:remote:media', @payload)
+      Redis.current.publish("timeline:public:remote:media:#{status.account.domain.mb_chars.downcase}", @payload)
     end
   end
 

app/services/remove_status_service.rb
@@ -144,6 +144,7 @@ class RemoveStatusService < BaseService
       redis.publish('timeline:public:local', @payload)
     else
       redis.publish('timeline:public:remote', @payload)
+      redis.publish("timeline:public:remote:#{@account.domain.mb_chars.downcase}", @payload)
     end
   end
 
@@ -155,6 +156,7 @@ class RemoveStatusService < BaseService
       redis.publish('timeline:public:local:media', @payload)
     else
       redis.publish('timeline:public:remote:media', @payload)
+      redis.publish("timeline:public:remote:media:#{@account.domain.mb_chars.downcase}", @payload)
     end
   end
 